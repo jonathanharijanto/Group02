e473058e471412d357f7092f7043d32295264d08
Taylor Kirkpatrick <tkirkpatrick@14four.com>
2016-01-21 20:45:58 -0800

    Added new method to Compare, added full scope of logical tests.

--- Assignment1/src/main/java/model/Compare.java
+++ Assignment1/src/main/java/model/Compare.java
@@ -6,7 +6,11 @@ package model;
 public class Compare {
     
 
-    public static boolean compare_suit(String a, String b){
+
+    // The two inputs should be the string values that represent the suits, or the faces (since they are also strings).
+    // Yeah, this is pretty much a stock function, or method or whatever it is in Java.
+    // This is called later in the greater compare function.
+    public static boolean same_string(String a, String b){
         if(a.equals(b)) {
             return true;
         } else {
@@ -14,6 +18,28 @@ public class Compare {
         }
     }
 
+    // Also rather simple, but hey, this is pretty good for hardly understanding Java.
+    // This one takes in three strings, two strings of numbers and one that asks whether user wants a greater than or less than comparison.
+    // Technically don't need the third one if this function was used cleverly, but let's be convenient.
+    // Number strings are parsed into ints and compared. Returns boolean.
+    // Also incorporated into the larger compare function.
+    public static boolean compare_face(String a, String b, String c){
+        int aint = Integer.parseInt(a);
+        int bint = Integer.parseInt(b);
+        if (c.equals("g")){
+            if (aint > bint){
+                return true;
+            }
+            return false;
+        }
+        if (c.equals("l")){
+            if (aint < bint){
+                return true;
+            }
+            return false;
+        }
+        return false;   // Something went wrong here. But logically it'll never happen, so don't worry boys and girls.
+    }
 
 
 
--- Assignment1/src/test/java/model/CompareTest.java
+++ Assignment1/src/test/java/model/CompareTest.java
@@ -2,6 +2,8 @@ package model;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.*;
 
@@ -14,5 +16,44 @@ public class CompareTest {
     public void testCompareExists(){
         Compare comp = new Compare();
         assertNotNull(comp);
+    }
+
+    @Test
+    public void testEqualSuit(){
+        Compare comp = new Compare();
+        assertEquals(true, comp.same_string("H","H"));
+    }
+    @Test
+    public void testInequalSuit(){
+        Compare comp = new Compare();
+        assertEquals(false, comp.same_string("H","D"));
+    }
+
+    // This test is meant to fail all the time, to make sure I'm actually making tests right. It should never pass.
+    @Test
+    public void testFailedEqual(){
+        Compare comp = new Compare();
+        assertEquals(true, comp.same_string("H","S"));
+    } // I'll comment it out when I'm finished with the feature.
+
+    @Test
+    public void testFirstNumBiggerTrue(){
+        Compare comp = new Compare();
+        assertEquals(true, comp.compare_face("13","8","g"));
+    }
+    @Test
+    public void testFirstNumBiggerFalse(){
+        Compare comp = new Compare();
+        assertEquals(false, comp.compare_face("2","8","g"));
+    }
+    @Test
+    public void testFirstNumSmallerTrue(){
+        Compare comp = new Compare();
+        assertEquals(true, comp.compare_face("4","5","l"));
+    }
+    @Test
+    public void testFirstNumSmallerFalse(){
+        Compare comp = new Compare();
+        assertEquals(false, comp.compare_face("5","4","l"));
     }
 }
