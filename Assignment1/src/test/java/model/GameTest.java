da76ee5223041bbfc89d1c9a3ba10a97ffcbc72b
SpiffiestOsprey <mcamwl@aol.com>
2016-01-22 16:30:51 -0800

    GameTest

--- Assignment1/src/test/java/model/GameTest.java
+++ Assignment1/src/test/java/model/GameTest.java
@@ -1,24 +1,26 @@
 package model;
 
 import org.junit.Test;
-
+import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by Cameron on 1/22/2016.
  */
 public class GameTest {
-/*
+
     @Test
     public void testDeal(){
-        Deck deck= new Deck();
+         Deck deck = new Deck();
 
-        Col col1 = new Col();
-        Col col2 = new Col();
-        Col col3 = new Col();
-        Col col4 = new Col();
+         Col col1 = new Col();
+         Col col2 = new Col();
+         Col col3 = new Col();
+         Col col4 = new Col();
 
-        assertEquals(col.topCard(), card1 );
+        Game game = new Game(deck, col1, col2, col3, col4);
 
-    }*/
+        assertNotNull(game.col1.topCard());
+
+    }
 }
