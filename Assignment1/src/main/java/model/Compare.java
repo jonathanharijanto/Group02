ba50c7431c44ec8d9ecfa7be519a950386c462d9
Taylor Kirkpatrick <tkirkpatrick@14four.com>
2016-01-22 17:07:17 -0800

    Finished compare functionality and compare tests.

--- Assignment1/src/main/java/model/Compare.java
+++ Assignment1/src/main/java/model/Compare.java
@@ -7,11 +7,11 @@ public class Compare {
     
 
 
-    // The two inputs should be the string values that represent the suits, or the faces (since they are also strings).
+    // The two inputs should be the int values that represent the suits. Would work fine with the faces, if I ever needed to test equality.
     // Yeah, this is pretty much a stock function, or method or whatever it is in Java.
     // This is called later in the greater compare function.
-    public static boolean same_string(String a, String b){
-        if(a.equals(b)) {
+    public static boolean same_int(int a, int b){
+        if(a == b) {
             return true;
         } else {
             return false;
@@ -19,21 +19,19 @@ public class Compare {
     }
 
     // Also rather simple, but hey, this is pretty good for hardly understanding Java.
-    // This one takes in three strings, two strings of numbers and one that asks whether user wants a greater than or less than comparison.
+    // This one takes in two ints, two ints representing the faces and one that asks if user wants a greater than or less than comparison.
     // Technically don't need the third one if this function was used cleverly, but let's be convenient.
-    // Number strings are parsed into ints and compared. Returns boolean.
+    // Returns boolean.
     // Also incorporated into the larger compare function.
-    public static boolean compare_face(String a, String b, String c){
-        int aint = Integer.parseInt(a);
-        int bint = Integer.parseInt(b);
-        if (c.equals("g")){
-            if (aint > bint){
+    public static boolean compare_face(int a, int b, String c){
+        if (c.equals("g")){     // The less than is never actually used in this scope. But who knows what the future holds?
+            if (a > b){
                 return true;
             }
             return false;
         }
         if (c.equals("l")){
-            if (aint < bint){
+            if (a < b){
                 return true;
             }
             return false;
@@ -41,7 +39,20 @@ public class Compare {
         return false;   // Something went wrong here. But logically it'll never happen, so don't worry boys and girls.
     }
 
+    public static boolean can_remove(Card a, Card b){
+        int asuit = a.getSuit();
+        int bsuit = b.getSuit();
+        int aface = a.getRank();
+        int bface = b.getRank();
 
+        if (same_int(asuit,bsuit)){
+            if (compare_face(aface,bface,"l")){
+                return true;
+            }
+        }
+        return false;
+
+    }
 
 
 }
--- Assignment1/src/test/java/model/CompareTest.java
+++ Assignment1/src/test/java/model/CompareTest.java
@@ -21,48 +21,66 @@ public class CompareTest {
     @Test
     public void testEqualSuit(){
         Compare comp = new Compare();
-        assertEquals(true, comp.same_string("H","H"));
+        assertEquals(true, comp.same_int(0,0));
     }
     @Test
     public void testInequalSuit(){
         Compare comp = new Compare();
-        assertEquals(false, comp.same_string("H","D"));
+        assertEquals(false, comp.same_int(0,1));
     }
 
-    // This test is meant to fail all the time, to make sure I'm actually making tests right. It should never pass.
-    @Test
-    public void testFailedEqual(){
-        Compare comp = new Compare();
-        assertEquals(true, comp.same_string("H","S"));
-    } // I'll comment it out when I'm finished with the feature.
+//    // This test is meant to fail all the time, to make sure I'm actually making tests right. It should never pass.
+//    @Test
+//    public void testFailedEqual(){
+//        Compare comp = new Compare();
+//        assertEquals(true, comp.same_int(0,1));
+//    } // I'll comment it out when I'm finished with the feature.
 
     @Test
     public void testFirstNumBiggerTrue(){
         Compare comp = new Compare();
-        assertEquals(true, comp.compare_face("13","8","g"));
+        assertEquals(true, comp.compare_face(13,8,"g"));
     }
     @Test
     public void testFirstNumBiggerFalse(){
         Compare comp = new Compare();
-        assertEquals(false, comp.compare_face("2","8","g"));
+        assertEquals(false, comp.compare_face(2,8,"g"));
     }
     @Test
     public void testFirstNumSmallerTrue(){
         Compare comp = new Compare();
-        assertEquals(true, comp.compare_face("4","5","l"));
+        assertEquals(true, comp.compare_face(4,5,"l"));
     }
     @Test
     public void testFirstNumSmallerFalse(){
         Compare comp = new Compare();
-        assertEquals(false, comp.compare_face("5","4","l"));
+        assertEquals(false, comp.compare_face(5,4,"l"));
     }
-    @Test
+
+    @Test   // These next two tests actually are unrelated to the compare part of this project. Just me testing making sure things work before I start using them.
     public void testCardRankCompare(){
         Card card = new Card(2,3);
         assertEquals(3,card.getRank());
     }
+    @Test
     public void testCardSuitCompare(){
         Card card = new Card(2,3);
         assertEquals(2,card.getSuit());
     }
+
+    @Test   // The big finisher.
+    public void testCanRemoveTrue(){
+        Card fivespades = new Card(1,5);
+        Card tenspades = new Card(1,10);
+        Compare comp = new Compare();
+        assertEquals(true, comp.can_remove(fivespades,tenspades));
+    }
+    @Test
+    public void testCanRemoveFalse(){
+        Card fivespades = new Card(1,5);
+        Card tenspades = new Card(1,10);
+        Compare comp = new Compare();
+        assertEquals(false, comp.can_remove(tenspades,fivespades));
+    }
+
 }
